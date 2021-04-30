a2d5aa894636047024181d6e39e6c6de475559f4
KDivya Swarnkar <KDivya.Swarnkar@gmail.com>
2021-04-30 12:24:50 +0530

    Get Book By Id In asp.net core webapi

--- BookStore.Api/Repository/BookRepository.cs
+++ BookStore.Api/Repository/BookRepository.cs
@@ -16,6 +16,7 @@ namespace BookStore.Api.Repository
             _context = context;
         }
     
+        // Get All Books 
         public async Task<List<BookModel>> GetAllBooksAsync()
         {
             var records = await _context.Books.Select(x => new BookModel()
@@ -24,6 +25,22 @@ namespace BookStore.Api.Repository
                 Title = x.Title,
                 Description = x.Description,
             }).ToListAsync();
+            return records;
+        }
+
+        // Get one item by id 
+        public async Task<BookModel> GetBookByIdAsync(int BookId)
+        {
+            // we can use _context.Books.FindAsync(BookId) from getting this by id
+            var records = await _context.Books.Where(x=>x.Id==BookId).Select(x => new BookModel()
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Description = x.Description,
+            }).FirstOrDefaultAsync();
+
+            // we can use firstAsync instead of FirstOrDefaultAsync (different between this is if you are lookin for the book if that book is not exist in
+            // repo/db then firstasync method give the error where FirstOrDefaultAsync give the null value)
             return records;
         }
     }
--- BookStore.Api/Repository/IBookRepository.cs
+++ BookStore.Api/Repository/IBookRepository.cs
@@ -7,5 +7,6 @@ namespace BookStore.Api.Repository
     public interface IBookRepository
     {
         Task<List<BookModel>> GetAllBooksAsync();
+        Task<BookModel> GetBookByIdAsync(int BookId);
     }
 }
