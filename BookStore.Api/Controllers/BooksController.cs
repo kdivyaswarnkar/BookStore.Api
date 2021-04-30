5ba656920e2bfa3a46774646ac54306567493a85
KDivya Swarnkar <KDivya.Swarnkar@gmail.com>
2021-04-30 12:11:39 +0530

    Create Api for get all items from the database

--- BookStore.Api/Controllers/BooksController.cs
+++ BookStore.Api/Controllers/BooksController.cs
@@ -1,15 +1,29 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BookStore.Api.Repository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
+// in this bookconroller class we have to get the data from book repository so we have to inject them in here so use constructor
 namespace BookStore.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class BooksController : ControllerBase
     {
+        private readonly IBookRepository _bookRepository;
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+        [HttpGet("")]
+        public async Task<IActionResult> GetAllBooks()
+        {
+            var books =await _bookRepository.GetAllBooksAsync();
+            return Ok(books);
+        }
     }
 }
--- BookStore.Api/Repository/IBookRepository.cs
+++ BookStore.Api/Repository/IBookRepository.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using BookStore.Api.Models;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookStore.Api.Repository
 {
     public interface IBookRepository
     {
+        Task<List<BookModel>> GetAllBooksAsync();
     }
 }
